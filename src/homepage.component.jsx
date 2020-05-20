f6f93b1c30ad13d0ac3caf6b31fea328cf73a671
aaron <aaron.wolf@gmail.com>
2020-05-20 11:17:38 +0300

    sass install, homepage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                    <span className='title'>shop now</span>
+                </div>
+            </div>
+        </div>        
+    </div>
+)
+
+export default Homepage

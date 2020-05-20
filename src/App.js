5a0f225f6c4a8e2d9b59fe82ce0d6264c493f8b7
aaron <aaron.wolf@gmail.com>
2020-05-20 11:27:06 +0300

    homepage css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component'
+import Homepage from './homepage.component';
+import './homepage.style.css';
 
 function App() {
   return (

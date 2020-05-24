35337ffcec4cb9584cb87bba9297a44cdb503d95
aaron <aaron.wolf@gmail.com>
2020-05-24 14:11:09 +0300

    shop page

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.style.scss';
-import Directory from '../../components/directory/directory.component'
 
-const Homepage = () => (
-    <div className='homepage'>
-        <Directory />
-    </div>
-)
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
 
-export default Homepage
+export default HomePage;
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,12 @@
+/* Used as starting data for ShopPage */
+
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: 'Hats',
+    routeName: 'hats',
+    items: []
+  }
+];
+
+export default SHOP_DATA;

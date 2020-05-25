fd4327cbf86eb9074c23d06c22d77847c3212124
aaron <aaron.wolf@gmail.com>
2020-05-25 10:49:16 +0300

    shop preview

--- src/components/preview/preview.component.jsx
+++ src/components/preview/preview.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './preview.style.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const Preview = ({title, items }) => (
     <div className='collection-preview'>
@@ -7,8 +8,8 @@ const Preview = ({title, items }) => (
         <div className='preview'> 
             { items
                 .filter((item, idx) => idx < 4 )
-                .map((item) => (
-                    <div key={item.id}>{item.name}</div>
+                .map(({ id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))}
         </div>
     </div>

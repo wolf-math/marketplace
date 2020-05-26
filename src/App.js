367f8f6dc7e37ecdc65a053f124b41be84977056
aaron <aaron.wolf@gmail.com>
2020-05-26 21:55:54 +0300

    sign in page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Navbar from './components/navbar/navbar.component'
+import Navbar from './components/navbar/navbar.component';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUp} />
       </Switch>
     </div>
   );
